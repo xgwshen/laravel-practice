6d7903b79fd03b3ad70cbc58189f819f818bfa46
xgwshen <xgwshen@gmail.com>
2018-04-22 22:30:48 +0800

    dev->dev;

--- xgwshen/my.php
+++ xgwshen/my.php
@@ -5,3 +5,5 @@ echo "hello world !";
 echo "something went wrong! we can do it throng self";
 //服务容器 中间件
 echo '11111';
+//为甚么分支里commit后,切换到master上,进行远程push后,切换来再进行push,本地上是有修改的,但是远程并没有修改?
+echo "dev->commit; checkout master; push master;checkout dev; push dev;local dev update;origin dev no;";
