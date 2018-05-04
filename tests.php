cd2b0d13399ed631757e9f72a5a7c453ab27e00c
xgwshen <xgwshen@gmail.com>
2018-05-04 14:26:45 +0800

    tijiao

---
+++ tests.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace ;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class Tests extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
